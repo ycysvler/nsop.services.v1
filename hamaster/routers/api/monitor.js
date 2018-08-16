fb7279b898db93459af82c62dd915c74e3e17222
zhanghongqing@interrcs.com <zhanghongqing@interrcs.com>
2018-08-16 18:17:23 +0800

    * 修改同步orgsource逻辑

--- hamaster/routers/api/monitor.js
+++ hamaster/routers/api/monitor.js
@@ -5,12 +5,27 @@
  */
 const moment = require('moment');
 const MonitorLogic = require('../../../db/mongo/dao/monitor');
+const OrganizationLogic = require('../../../db/mongo/dao/organization');
 
 module.exports = function (router) {
 
     router.get('/monitor', async(ctx)=>{
+        let orgMap = {};
+        let ologic = new OrganizationLogic();
+        let oitems = await ologic.list();
+        for(let item of oitems){
+            orgMap['orgid.'+item.orgid] = item;
+        }
+
         let logic = new MonitorLogic();
         let items = await logic.list();
-        ctx.body = {code: 200, data: items};
+
+        let result = [];
+        for(let item of items){
+            let org = orgMap['orgid.' + item.orgid];
+            result.push({orgid: org.orgid, name:org.name, host:org.host, type:item.type, updatetime:item.updatetime});
+        }
+
+        ctx.body = {code: 200, data: result};
     });
 };

999a71d9d0e625b4dbef7b8b5bacce693677574e
ycysvler <ycysvler@hotmail.com>
2018-09-08 17:04:17 -0700

    up

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Hello World !" 
-pm2 start ./basedata/server.js --name='basedata-4997'
-pm2 start ./model/server.js --name='model-4001'
-pm2 start ./datasync/server.js --name='datasync-4998'
-pm2 start ./hamaster/server.js --name='hamaster-4999'
+pm2 start ./basedata/server.js --name='basedata:4997'
+pm2 start ./model/server.js --name='model:4001'
+pm2 start ./datasync/server.js --name='datasync:4998'
+pm2 start ./hamaster/server.js --name='hamaster:4999'

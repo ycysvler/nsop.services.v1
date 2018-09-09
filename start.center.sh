5c196e5e50ff369874653211231b87c7aaa6d9e6
VLER <ycysvler@hotmail.com>
2018-09-09 09:51:25 +0800

    * add dialing

---
+++ start.center.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hello World !" 
+pm2 start ./basedata/server.js --name='api:basedata:4997'
+pm2 start ./model/server.js --name='api:model:4001'
+pm2 start ./datasync/server.js --name='api:datasync:4998'
+pm2 start ./hamaster/server.js --name='api:hamaster:4999'
+pm2 start ./hamaster/services/dialing.js --name='service:dialing'
+pm2 start ../nsop.web.v1/server/server.js --name:'web:manager:4010'
